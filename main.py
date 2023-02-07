f24b54e11da6e6931c59bedc7e0370059b6b55e3
NvanNov <loginnovickoff@yandex.ru>
2023-02-07 19:50:36 +0300

    Add gui.ui and create code

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from random import randint
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gui.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+        self.do_paint = False
+
+    def draw_flag(self, qp):
+        radius = randint(1, 200)
+        qp.setBrush(QColor(randint(1, 255), randint(1, 255), randint(1, 255)))
+        qp.drawEllipse(randint(radius, 400) - radius, randint(radius, 300) - radius, radius, radius)
+
+
+def my_exception_hook(exctype, value, traceback):
+    print(exctype, value, traceback)
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+
+sys._excepthook = sys.excepthook
+sys.excepthook = my_exception_hook
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
